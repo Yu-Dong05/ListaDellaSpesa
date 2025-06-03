8aa7ca655abcdc7fa4f3f5f460f8603068f58d20
Yu-Dong05 <yu.dong0502.education@gmail.com>
2025-06-03 12:28:28 +0200

    update

--- FE/myapp/svelte.config.js
+++ FE/myapp/svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
-const config = { kit: { adapter: adapter() } };
-
-export default config;
+export default {
+    kit: {
+        adapter: adapter({
+            // see below for options that can be set here
+        })
+    }
+};
